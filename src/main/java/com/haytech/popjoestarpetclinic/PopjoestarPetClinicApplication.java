481be54a5f2946d3c9c2f0588ea7616a8cfc0f77
PopJoestar <andrianjafygaetan@gmail.com>
2021-10-12 12:40:41 +0300

    Initial project Add

---
+++ src/main/java/com/haytech/popjoestarpetclinic/PopjoestarPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.haytech.popjoestarpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopjoestarPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopjoestarPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haytech/popjoestarpetclinic/PopjoestarPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haytech.popjoestarpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopjoestarPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
